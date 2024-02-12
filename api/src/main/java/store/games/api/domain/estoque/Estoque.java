c6f01dbb57a656eb48820dc2179969712eb3c5ff
Dayne <daynesouza@hotmail.com.br>
2024-02-12 12:30:10 -0300

    Realizado o mapeamento da entidade "produtos" no banco de dados.

---
+++ api/src/main/java/store/games/api/domain/estoque/Estoque.java
@@ -0,0 +1,36 @@
+package store.games.api.domain.estoque;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import store.games.api.domain.product.Product;
+
+import java.util.Date;
+
+@Table(name = "estoque")
+@Entity(name = "estoque")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Estoque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produto_cpf")
+    private Product produto;
+
+    @Column(name = "data_referencia")
+    private Date data;
+
+    private int quantidade;
+
+    public Estoque( Product produto, int quantidade, Date data){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.data = data;
+    }
+}
